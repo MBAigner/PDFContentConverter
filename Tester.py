cb158b1f122f58e2fb16b9af61a19a3691f40731
Michael Aigner <Michael.Aigner-CIC@ibm.com>
2020-07-26 19:37:09 +0200

    tester + example document

---
+++ Tester.py
@@ -0,0 +1,14 @@
+from PDFContentConverter import PDFContentConverter
+from util import constants
+from util import StorageUtil
+
+# convert PDF
+pdf = "eu-001.pdf"
+file = constants.PDF_PATH + pdf
+converter = PDFContentConverter(file)
+result, media_boxes = converter.convert()
+
+# store results
+csv = constants.CSV_PATH + StorageUtil.replace_file_type(pdf, "csv")
+result.to_csv(csv, sep=";")
+StorageUtil.save_object(media_boxes, constants.CSV_PATH, StorageUtil.cut_file_type(pdf))
